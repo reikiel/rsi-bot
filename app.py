a2a0435bd57c7f2ef66468ae0667560a5149adf5
Valerie Tan <valerietan7@gmail.com>
2021-05-05 01:30:56 +0800

    order logic, checkqty, checkposition

--- app.py
+++ app.py
@@ -8,16 +8,78 @@ RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'UNIUSDT'
-TRADE_USDT_AMOUNT = 1000
+TRADE_ASSET = 'UNI' # rmb change to uni
+BASE_ASSET = 'USDT'
+TRADE_BASE_AMOUNT = 1000
 
 closes = []
 in_position = False
 
 client = Client(config.API_KEY, config.API_SECRET)
 
+# check how much holdings
+def check_qty(asset):
+  balance = client.get_asset_balance(asset=asset)
+  return float(balance['free'])
+  
+
+# On start, check if in position in case program closes
+def check_in_position():
+  global in_position
+  if check_qty(TRADE_ASSET) > 2:
+    in_position = True
+  else:
+    in_position = False
+
+check_in_position()
+
+# send order to binance
+# Change all to create_order when done
+def order(side, qty, symbol, order_type=ORDER_TYPE_MARKET):
+  # if sell, sell all qty # of coins => use quantity
+  if side == SIDE_SELL:
+    try:
+      order = client.create_test_order(
+      symbol=symbol,
+      side=side,
+      type=ORDER_TYPE_MARKET,
+      quantity=qty)
+      return order
+    except Exception as e:
+      # TODO: log error
+      return False
+
+  # if buy, buy qty WORTH (USDT) of coin => use quoteOrderQty
+  elif side == SIDE_BUY:
+    try: 
+      order = client.create_test_order(
+      symbol=symbol,
+      side=side,
+      type=ORDER_TYPE_MARKET,
+      quoteOrderQty=qty)
+      return order
+    except Exception as e:
+      # TODO: log error
+      return False
+
+
+# check profit before selling - estimated profit
+def check_profit_before():
+  return
+
+# see profit after selling - real profit accounting for fees
+# can try to get trade history and see how much i buy at, and compare with the current sell order
+# might need to get full response? use newOrderRespType
+# calculate fill orders
+# then minus TRADE_USDT_AMOUNT
+def check_profit_after(order):
+  return
+
+########################## WebSocket Functions ##########################
 def on_open(ws):
   print('connection opened')
-
+  # print(in_position)
+  
 def on_close(ws):
   print('closed connection')
 
@@ -47,15 +109,15 @@ def on_message(ws, message):
         if not in_position:
           print("You are not in position, can't sell")
         else:
-          # TODO: Check profit after fees
-          # TODO: Check quantity
-          sell_quantity = check_qty()
+          # TODO: Check profit after fees (before selling)
+          sell_quantity = check_qty(TRADE_ASSET)
           # Sell order logic - check if minimum profit is met
           print("RSI is overbought and your current balance is profiting. Selling...")
           order_success = order(SIDE_SELL, sell_quantity, TRADE_SYMBOL)
-          if order_success:
+          if order_success != False:
             in_position = False
             print("SELL order for {} UNI success!".format(sell_quantity))
+            # TODO: Check real profit from order response (after selling)
           else:
             print("SELL order not successful, please check what is the problem")
     
@@ -65,36 +127,17 @@ def on_message(ws, message):
         else:
           print("RSI is oversold and you are not in position. Buying...")         
           # Buy Order logic
-          order_success = order(SIDE_BUY, TRADE_USDT_AMOUNT, TRADE_SYMBOL)
-          if order_success:
+          base_amt = check_qty(BASE_ASSET)
+          buy_amt = min(base_amt, TRADE_BASE_AMOUNT)
+          order_success = order(SIDE_BUY, buy_amt, TRADE_SYMBOL)
+          if order_success != False:
             in_position = True
             # TODO: Check quantity from order for logging? or just use check_qty?
-            buy_quantity = check_qty()
+            buy_quantity = check_qty(TRADE_ASSET)
             print("BUY order for {} UNI success!".format(buy_quantity))
           else:
             print("BUY order not successful, please check what is the problem")
 
-
-# send order to binance
-def order(side, qty, symbol, order_type=ORDER_TYPE_MARKET):
-  # if sell, sell all qty => use quantity
-  # if buy, buy qty WORTH (USDT) of coin => use quoteOrderQty
-  return True
-
-# check profit before selling
-def check_profit_before():
-  return
-
-# see profit after selling
-# might need to get full response? use newOrderRespType
-# calculate fill orders
-# then minus TRADE_USDT_AMOUNT
-def check_profit_after():
-  return
-
-# check how much holdings
-def check_qty():
-  return 1
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
