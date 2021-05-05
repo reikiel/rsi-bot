60a0ba6cbd513fe479ec74ce6a3ae572e57661b6
Valerie Tan <valerietan7@gmail.com>
2021-05-06 06:36:20 +0800

    added logging

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import websocket, json, pprint, talib, numpy
+import websocket, json, pprint, talib, numpy, logging
 import config
 from binance.client import Client
 from binance.enums import *
@@ -15,9 +15,13 @@ BINANCE_FEE = 0.1/100 # 0.1% of the TRADE_ASSET
 
 closes = []
 buy_amt = 1000
+total_session_profit = 0
 in_position = False
 
 client = Client(config.API_KEY, config.API_SECRET)
+
+# Logging
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 # check how much holdings
 def check_qty(asset):
@@ -51,7 +55,7 @@ def order(side, qty, symbol, order_type=ORDER_TYPE_MARKET):
       quantity=qty)
       return order
     except Exception as e:
-      # TODO: log error
+      logging.exception("SELL error occurred")
       return False
 
   # if buy, buy qty WORTH (USDT) of coin => use quoteOrderQty
@@ -65,7 +69,7 @@ def order(side, qty, symbol, order_type=ORDER_TYPE_MARKET):
       quoteOrderQty=qty)
       return order
     except Exception as e:
-      # TODO: log error
+      logging.exception("BUY error occurred")
       return False
 
 
@@ -77,6 +81,7 @@ def check_profit_before(price, sell_qty):
   est_fee_buy = buy_amt * BINANCE_FEE
   est_fee_sell = total_from_sale * BINANCE_FEE
   profit = total_from_sale - est_fee_sell - est_fee_buy
+  logging.info("Checking to sell {} UNI for an estimated of ${} profit".format(sell_qty, profit))
   return profit
 
 # see profit after selling - real profit accounting for fees
@@ -85,12 +90,18 @@ def check_profit_before(price, sell_qty):
 # calculate fill orders
 # then minus TRADE_USDT_AMOUNT
 def check_profit_after(order):
+  global total_session_profit
   fills = order['fills']
-  total = 0
+  total_money = 0
+  total_qty = 0
   for fill in fills:
+    total_qty += float(fill['qty'])
     temp = float(fill['price']) * float(fill['qty']) - float(fill['commission'])
-    total += temp
-  profit = total - buy_amt 
+    total_money += temp
+  profit = total_money - buy_amt 
+  logging.info("Sold {} UNI for a ${} profit".format(total_qty, profit))
+  total_session_profit += profit
+  print("Total profit from this session is ${}!".format(total_session_profit))
   return profit
 
 ########################## WebSocket Functions ##########################
@@ -119,16 +130,18 @@ def on_message(ws, message):
     if len(closes) > RSI_PERIOD:
       np_closes = numpy.array(closes)
       rsi = talib.RSI(np_closes, RSI_PERIOD)
-      print("All rsi calculated so far: {}".format(rsi))
+      # print("All rsi calculated so far: {}".format(rsi))
       current_rsi = rsi[-1]
-      print("The current RSI is {}".format(current_rsi))
+      # print("The current RSI is {}".format(current_rsi))
 
       if current_rsi > RSI_OVERBOUGHT:
+        logging.info("Current RSI is {}, overbought.".format(current_rsi))
+        print("Current RSI is {}, overbought.")
         if not in_position:
+          logging.info("You are not in position, can't sell")
           print("You are not in position, can't sell")
         else:
           # Check estimated profit after fees, before selling
-          # TODO: Log estimated profit to compare with actual profit
           sell_quantity = check_qty(TRADE_ASSET)
           est_profit = check_profit_before(float(close), sell_quantity)
           # Sell order logic - check if minimum profit is met
@@ -139,15 +152,17 @@ def on_message(ws, message):
               in_position = False
               print("SELL order for {} UNI success!".format(sell_quantity))
               # Check real profit from order response (after selling)
-              # TODO: Log real profit after selling (in or out of function)
               act_profit = check_profit_after(order_success)
             else:
               print("SELL order not successful, please check what is the problem")
           else:
+            logging.info("Current profit is not minimum, not selling.")
             print("RSI is overbought BUT your current balance is not profiting. Hold.")
     
       elif current_rsi < RSI_OVERSOLD:
+        logging.info("Current RSI is {}, oversold.".format(current_rsi))
         if in_position:
+          logging.info("You are already in a position, can't buy")
           print("You are already in a position, can't buy")
         else:
           print("RSI is oversold and you are not in position. Buying...")         
@@ -157,8 +172,8 @@ def on_message(ws, message):
           order_success = order(SIDE_BUY, buy_amt, TRADE_SYMBOL)
           if order_success != False:
             in_position = True
-            # TODO: Check quantity from order for logging? or just use check_qty?
             buy_quantity = check_qty(TRADE_ASSET)
+            logging.info("Bought {} UNI for ${}".format(buy_quantity, buy_amt))
             print("BUY order for {} UNI success!".format(buy_quantity))
           else:
             print("BUY order not successful, please check what is the problem")
